4e268825de28eb428df38721b166fbee3a595b78
Tonesandhi <alan21wyf@gmail.com>
2021-09-22 03:07:29 -0500

    4 Practice Questions

---
+++ C#/Practice/ConsoleApp1/ConsoleApp1/Operations.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Operations
+    {
+        public static int Factorial(int s)
+        {
+            if (s <= 1)
+            {
+                return 1;
+            }
+            return s * Factorial(s - 1);
+        }
+
+        public static bool IsPrime(int s)
+        {
+            if (s <= 2)
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = 2; i < (s/2)+1; i++)
+                {
+                    if (s % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        public static bool IsLeapYear(int s)
+        {
+            if (s%4 != 0)
+            {
+                return false;
+            }
+            else
+            {
+                if (s%100 == 0)
+                {
+                    if (s%400 == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public static int LCM(int a, int b)
+        {
+            return (a / GCD(a, b)) * b;
+        }
+
+        public static int GCD(int a, int b)
+        {
+            if (a == 0)
+            {
+                return b;
+            }
+            return GCD(b % a, a);
+        }
+    }
+}
---
+++ C#/Practice/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Operations.Factorial(5));
+            Console.WriteLine(Operations.IsPrime(s:2));
+            Console.WriteLine(Operations.IsLeapYear(1900));
+            Console.WriteLine(Operations.LCM(5, 25));
+        }
+    }
+}
